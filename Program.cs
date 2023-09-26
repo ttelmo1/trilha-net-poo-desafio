a9dac2cdac4dacf1916ab48f1b472c92879bd41a
Telmo <telmobsj@gmail.com>
2023-09-26 20:37:31 -0300

    base

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("999999999", "Nokia 3310", "123456789", 16);
+Iphone iphone = new Iphone("999999999", "Iphone 11", "987654321", 64);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
